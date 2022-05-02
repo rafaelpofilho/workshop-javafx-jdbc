b06a98ae09dd0aa89cacf983bac896688846ebf6
Rafael Filho <soft_rafael@hotmail.com>
2022-05-02 19:07:03 -0300

    Main View

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,5 +1,5 @@
 package application;
-	
+
 import java.io.IOException;
 
 import javafx.application.Application;
@@ -8,20 +8,22 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-
 public class Main extends Application {
+	
 	@Override
-	public void start(Stage stage) {
+	public void start(Stage primaryStage) {
 		try {
-			Parent parent = FXMLLoader.load(getClass().getResource("/gui/View.fxml"));
-			Scene scene = new Scene(parent);
-			stage.setScene(scene);
-			stage.show();
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("/gui/MainView.fxml"));
+			Parent parent = loader.load();
+			Scene mainScene = new Scene(parent);
+			primaryStage.setScene(mainScene);
+			primaryStage.setTitle("Sample JavaFX application");
+			primaryStage.show();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
